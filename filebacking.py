ef88393fd16cd0de10ae823b5a97ac9c283de769
XxArnavxX <60574509+XxArnavxX@users.noreply.github.com>
2021-06-28 17:45:19 +0530

    Add files via upload

---
+++ filebacking.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+source = input("Enter source folder name: ")
+destination = input("Enter destination folder name: ")
+
+source = source + '/'
+destination = destination + '/'
+
+listOfFiles = os.listdir(source)
+print(listOfFiles)
+
+for file in listOfFiles:
+    shutil.copy((source + file), destination)
---
+++ orgfile.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = input("Enter the path of the folder that you want to sort: ")
+listOfFiles = os.listdir(path)
+for file in listOfFiles:
+    name, ext = os.path.splitext(file)
+    ext = ext[1:]
+    if ext == '':
+        continue
+
+    if os.path.exists(path + '/' + ext):
+        shutil.move(path + '/' + file, path + '/' + ext + '/' + file)
+
+    else:
+        os.makedirs(path + '/' + ext)
+        shutil.move(path + '/' + file, path + '/' + ext + '/' + file)
